0de12ac78442f3201d20ce66f5a40167576aebd5
LiLi <jroc050988@gmail.com>
2021-05-02 17:26:17 +0800

    active header.resume--finish

--- js/active.js
+++ js/active.js
@@ -1,6 +1,26 @@
+new WOW().init();
 
+
+$(".skillList ul").addClass("none");
+$(".scroll").mouseenter(function (e) { 
+    // values: e.clientX, e.clientY, e.pageX, e.pageY
+    $(".scroll span").addClass("spanhover");
+});
+$(".scroll").mouseleave(function (e) { 
+    // values: e.clientX, e.clientY, e.pageX, e.pageY
+    $(".scroll span").removeClass("spanhover");
+});
 $(".skillList .tittle").click(function (e) { 
     e.preventDefault();
-    $(".skillList ul").toggleClass("none");
+    $(this).parent().siblings().find('ul').slideUp();
+    $(this).siblings('ul').slideToggle();
+    
 });
-$(".skillList ul").addClass("none");
+$(window).scroll(function () { 
+    var scrollVal = $(this).scrollTop();
+    console.log(scrollVal);
+    if(scrollVal >= 600){
+        $(".myphoto").slideDown(1000);
+        $(".introduction").delay(1000).fadeIn(800);
+    }
+});
