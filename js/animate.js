f38f265c1e68daf55eef87514783a1d1c51e0648
LiLi <jroc050988@gmail.com>
2021-05-02 18:51:30 +0800

    active skill--finish

---
+++ js/animate.js
@@ -0,0 +1,28 @@
+new WOW().init();
+
+$(".skillList ul").addClass("none");
+$(".scroll").mouseenter(function (e) { 
+    // values: e.clientX, e.clientY, e.pageX, e.pageY
+    $(".scroll span").addClass("spanhover");
+});
+$(".scroll").mouseleave(function (e) { 
+    // values: e.clientX, e.clientY, e.pageX, e.pageY
+    $(".scroll span").removeClass("spanhover");
+});
+$(".skillList .tittle").click(function (e) { 
+    e.preventDefault();
+    $(this).parent().siblings().find('ul').slideUp();
+    $(this).siblings('ul').slideToggle();
+    
+});
+$(window).scroll(function () { 
+    var scrollVal = $(this).scrollTop();
+    console.log(scrollVal);
+    if(scrollVal >= 600){
+        $(".myphoto").slideDown(1000);
+        $(".introduction").delay(1000).fadeIn(800);
+    }
+    if(scrollVal >= 1440){
+        $(".graph_circle").addClass("part1");
+    }
+});
